7f1ac7e18353df018390b4b9a9ba2b478a62688a
Ethan Niv <en2456@log-3.hpc.nyu.edu>
2025-12-19 01:42:24 -0500

    Finished Implementing Baseline

--- source/rob6323_go2/rob6323_go2/tasks/direct/rob6323_go2/rob6323_go2_env_cfg.py
+++ source/rob6323_go2/rob6323_go2/tasks/direct/rob6323_go2/rob6323_go2_env_cfg.py
@@ -17,6 +17,8 @@ from isaaclab.sensors import ContactSensorCfg
 from isaaclab.markers import VisualizationMarkersCfg
 from isaaclab.markers.config import BLUE_ARROW_X_MARKER_CFG, FRAME_MARKER_CFG, GREEN_ARROW_X_MARKER_CFG
 
+from isaaclab.actuators import ImplicitActuatorCfg
+
 @configclass
 class Rob6323Go2EnvCfg(DirectRLEnvCfg):
     # env
@@ -25,7 +27,8 @@ class Rob6323Go2EnvCfg(DirectRLEnvCfg):
     # - spaces definition
     action_scale = 0.25
     action_space = 12
-    observation_space = 48
+    observation_space = 48 + 4  # Added 4 for clock inputs for the Raibert Heuristic step
+    # observation_space - old: before adding clock inputs for Raibert Heuristic: 48
     state_space = 0
     debug_vis = True
 
@@ -56,6 +59,16 @@ class Rob6323Go2EnvCfg(DirectRLEnvCfg):
     )
     # robot(s)
     robot_cfg: ArticulationCfg = UNITREE_GO2_CFG.replace(prim_path="/World/envs/env_.*/Robot")
+    
+    # "base_legs" is an arbitrary key we use to group these actuators
+    robot_cfg.actuators["base_legs"] = ImplicitActuatorCfg(
+        joint_names_expr=[".*_hip_joint", ".*_thigh_joint", ".*_calf_joint"],
+        effort_limit=23.5,
+        velocity_limit=30.0,
+        stiffness=0.0,  # CRITICAL: Set to 0 to disable implicit P-gain
+        damping=0.0,    # CRITICAL: Set to 0 to disable implicit D-gain
+    )
+
 
     # scene
     scene: InteractiveSceneCfg = InteractiveSceneCfg(num_envs=4096, env_spacing=4.0, replicate_physics=True)
@@ -79,3 +92,36 @@ class Rob6323Go2EnvCfg(DirectRLEnvCfg):
     # reward scales
     lin_vel_reward_scale = 1.0
     yaw_rate_reward_scale = 0.5
+    action_rate_reward_scale = -0.1
+    
+    
+    # PD control gains
+    Kp = 20.0  # Proportional gain
+    Kd = 0.5   # Derivative gain
+    torque_limits = 100.0  # Max torque
+    
+    base_height_min = 0.20  # Terminate if base is lower than 20cm
+
+
+    # Raibert Heuristic - Step 4
+    raibert_heuristic_reward_scale = -10.0
+    feet_clearance_reward_scale = -30.0
+    tracking_contacts_shaped_force_reward_scale = 4.0
+    
+    #Step 5
+    '''To achieve stable and natural-looking locomotion, we need to shape the robot's behavior further. We will add penalties for:
+
+    Non-flat body orientation (projected gravity).
+    Vertical body movement (bouncing).
+    Excessive joint velocities.
+    Body rolling and pitching (angular velocity).'''
+    
+    orient_reward_scale = -5.0 # Penalty for non-flat body orientation
+    lin_vel_z_reward_scale = -0.02 # Penalty for vertical body movement
+    dof_vel_reward_scale = -0.0001 # Penalty for excessive joint velocities ######
+    ang_vel_xy_reward_scale = -0.001 # Penalty for body rolling and pitching
+    
+    # Step 6
+    foot_clearance_reward_scale = -10.0
+    desired_foot_clearance = 0.08  # Desired foot clearance height in meters
+    tracking_contacts_shaped_force_reward_scale = 0.01 # 
